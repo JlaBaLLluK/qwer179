cf0ef8a5be11b85da9b660fef9aa82031ffd11d9
Egor <etkachev398@gmail.com>
2023-10-21 13:13:30 +0300

    Added template for adding data and function for it rendering

---
+++ project/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/add-data/', methods=['POST', 'GET'])
+def hello_world():
+    if request.method == 'GET':
+        return render_template('add_data_page.html')
+
+
+if __name__ == '__main__':
+    app.run()
