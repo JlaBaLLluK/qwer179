472927209bb8ba11dc627ce412010f9c616a37fa
Egor <etkachev398@gmail.com>
2023-10-21 20:55:26 +0300

    Added script.js file

--- project/main.py
+++ project/main.py
@@ -8,6 +8,10 @@ def hello_world():
     if request.method == 'GET':
         return render_template('add_data_page.html')
 
+    data_from_form = request.form['name']
+    print(request.form.keys())
+    return data_from_form
+
 
 if __name__ == '__main__':
     app.run()
---
+++ project/static/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('#add-field-button').click(function() {
+    var clonedField = $('#fields input:last').clone();
+    clonedField.attr('name', 'field' + ($('#fields input').length + 1));
+    $('#fields').append(clonedField);
+  });
+});
