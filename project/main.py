2fb8492dfc6d49a900b9bfac88fd0b9daf875b60
Egor <etkachev398@gmail.com>
2023-10-22 13:30:00 +0300

    Added the ability to save data in db and display it on the page

--- project/main.py
+++ project/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import psycopg2
 import os
 from dotenv import load_dotenv
+import json
 
 app = Flask(__name__)
 load_dotenv()
@@ -19,11 +20,24 @@ connection.commit()
 
 
 @app.route('/add-data/', methods=['POST', 'GET'])
-def hello_world():
-    if request.method == 'GET':
-        return render_template('add_data_page.html')
+def add_data():
+    if request.method == 'POST':
+        data = json.dumps(request.form)
+        cursor.execute("INSERT INTO data_table (data) VALUES (%s)", (data,))
+        connection.commit()
 
-    return "response"
+    return render_template('add_data_page.html')
+
+
+@app.route('/get-data/')
+def get_data():
+    cursor.execute("SELECT * FROM data_table;")
+    data_from_table = cursor.fetchall()
+    data = {}
+    for row in data_from_table:
+        data[str(row[0])] = row[1]
+
+    return render_template("all_data_page.html", data=data)
 
 
 if __name__ == '__main__':
