562d7dd8bb05471d3ec8e9f0cd3cdc30034ce3f5
Egor <etkachev398@gmail.com>
2023-10-22 11:43:10 +0300

    Added connection to postgresql db and table creating there

--- project/main.py
+++ project/main.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request
+import psycopg2
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+load_dotenv()
+connection = psycopg2.connect(
+    host="localhost",
+    database="task_db",
+    user=os.environ["DB_USER"],
+    password=os.environ["DB_PASSWORD"]
+)
+cursor = connection.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS data_table("
+               "id serial primary key,"
+               "data jsonb);")
+connection.commit()
 
 
 @app.route('/add-data/', methods=['POST', 'GET'])
@@ -13,3 +28,5 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run()
+    cursor.close()
+    connection.close()
